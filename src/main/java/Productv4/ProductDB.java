0723834e6f52331845140f41f39ac21416a1372f
DERVISEMREDEMIR <g00427391@atu.ie>
2024-12-02 11:48:49 +0000

    Final changes has been made

--- src/main/java/Productv4/ProductDB.java
+++ src/main/java/Productv4/ProductDB.java
@@ -5,68 +5,62 @@ public class ProductDB {
     //  public static Book or Software getProduct(String productCode) {
     public static Product getProduct(String productCode) {
 
-        Tv myTv = null;
+        Product myProduct = null;
+
         if (productCode.equalsIgnoreCase("Sam")) {
-            myTv = new Tv();
+            Tv myTv = new Tv();
             myTv.setCode(productCode);
             myTv.setDescription("Visual Studios");
             myTv.setPrice(57.50);
             myTv.setManufacture("Samsung");
             myTv.setScreenSize("43 Inch");
+            myProduct = myTv;
         } else if (productCode.equalsIgnoreCase("Tos")) {
-            myTv = new Tv();
+            Tv myTv = new Tv();
             myTv.setCode(productCode);
             myTv.setDescription("Visual Studios");
             myTv.setPrice(57.50);
             myTv.setManufacture("Toshiba");
             myTv.setScreenSize("55 Inch");
+            myProduct = myTv;
         } else if (productCode.equalsIgnoreCase("Lg")) {
-            myTv = new Tv();
+            Tv myTv = new Tv();
             myTv.setCode(productCode);
             myTv.setDescription("Visual Studios");
             myTv.setPrice(57.50);
             myTv.setManufacture("LG");
             myTv.setScreenSize("66 Inch");
-
+            myProduct = myTv;
         }
 
-        return myTv;
-    }
-
-
-    public static Music getMusic(String productCode) {
-
-        Music myMusic = null;
-        if (productCode.equalsIgnoreCase("PINK")) {
-            myMusic = new Music();
+        else if (productCode.equalsIgnoreCase("PINK")) {
+            Music myMusic = new Music();
             myMusic.setCode(productCode);
             myMusic.setDescription("Visual Studios");
             myMusic.setPrice(57.50);
             myMusic.setArtist("Pink Floyd");
             myMusic.setLabel("Columbia Group");
+            myProduct = myMusic;
         } else if (productCode.equalsIgnoreCase("PARK")) {
-            myMusic = new Music();
+            Music myMusic = new Music();
             myMusic.setCode(productCode);
             myMusic.setDescription("Build Java apps");
             myMusic.setPrice(57.50);
             myMusic.setArtist("LinkinPark");
             myMusic.setLabel("Warner Records");
+            myProduct = myMusic;
         } else if (productCode.equalsIgnoreCase("METAL")) {
-            myMusic = new Music();
+            Music myMusic = new Music();
             myMusic.setCode(productCode);
             myMusic.setDescription("Latest MySQL");
             myMusic.setPrice(54.50);
             myMusic.setArtist("Metallica");
             myMusic.setLabel("Metallica Labeler");
-
+            myProduct = myMusic;
         }
 
-        return myMusic;
+        return myProduct;
     }
-    // In a more realistic application, this code would get the data for the product from a file or database
-    // For now, this code just uses if/else statements to return the correct product data
 
-    //We need all the book and software objects, but what kind of object do we return?
-    //}
 }
 
