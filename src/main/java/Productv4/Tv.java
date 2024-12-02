1c755e8da3dcd187d14fce8a1670376c5832981d
DERVISEMREDEMIR <g00427391@atu.ie>
2024-12-02 11:09:48 +0000

    Tv tostring method added

--- src/main/java/Productv4/Tv.java
+++ src/main/java/Productv4/Tv.java
@@ -1,4 +1,36 @@
 package Productv4;
 
 public class Tv extends Product{
+
+    private String screenSize;
+    private String manufacture;
+
+    public Tv()
+    {
+        super();
+        screenSize = " ";
+        manufacture = " ";
+        count++;
+    }
+
+    public String getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(String screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " by " + manufacture + "\n" + "Screen Size is: \n" + screenSize;
+    }
 }
